bc93c6804c4fbe28e16660ddf0f46d0b4756168f
Gamel James <gamel@meezyart.com>
2019-02-21 21:05:01 -0800

    Clean up

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -3,20 +3,12 @@ import './App.css';
 import Calendar from './Calender';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      releases: null,
-      fetching: true
-    };
-  }
 
- 
 
   render() {
     return (
       <div className="App">
-        <Calendar releases={this.state.releases}/>
+        <Calendar/>
       </div>
     );
   }
--- react-ui/src/Calender.js
+++ react-ui/src/Calender.js
@@ -2,8 +2,8 @@ import React from "react";
 import m from "moment";
 import { clone } from "lodash";
 
-import "./Calender.css"
-import Logo from  "./assets/Netflix_Logo_RGB.png"
+import "./Calender.css";
+import Logo from "./assets/Netflix_Logo_RGB.png";
 
 class Calendar extends React.Component {
   state = {
@@ -14,9 +14,8 @@ class Calendar extends React.Component {
     fetching: true
   };
 
-
   componentDidMount() {
-    fetch('/releases')
+    fetch("/releases")
       .then(response => {
         if (!response.ok) {
           throw new Error(`status ${response.status}`);
@@ -28,30 +27,26 @@ class Calendar extends React.Component {
           releases: json.data,
           fetching: false
         });
-      }).catch(e => {
+      })
+      .catch(e => {
         this.setState({
           releases: `API call failed: ${e}`,
           fetching: false
         });
-      })
+      });
   }
-
-
 
   renderHeader = () => {
     return (
       <div className="header">
         <div className="prev">
-          <div className="icon" onClick={this.prev}>
-          </div>
+          <div className="icon" onClick={this.prev} />
         </div>
         <div className="month-header">
-
           <h1>{m(this.state.currentMonth).format("MMMM  YYYY")}</h1>
         </div>
         <div className="next">
-          <div className="icon" onClick={this.next}>
-          </div>
+          <div className="icon" onClick={this.next} />
         </div>
       </div>
     );
@@ -68,11 +63,12 @@ class Calendar extends React.Component {
     });
   };
 
-  findRelease = (date) => {
-    const formattedDate = m(date).format('L')
-    // console.log( formattedDate)
-    return this.state.releases.filter(node => m(node.launch_date).format('L') === formattedDate)
-  }
+  findRelease = date => {
+    const formattedDate = m(date).format("L");
+    return this.state.releases.filter(
+      node => m(node.launch_date).format("L") === formattedDate
+    );
+  };
 
   renderDay = (date, month) => {
     const day = {
@@ -85,10 +81,11 @@ class Calendar extends React.Component {
       isToday: m(date).isSame(new Date(), "day"),
       date: m(date)
     };
-    // console.log("day",  this.findRelease(date));
     let monthDiff = day.isCurrentMonth ? "" : " month-diff",
       today = day.isToday ? " is-today" : "",
-      hasRelease = day.hasReleaseToday.length  > 0 ? " release-today" : "";
+      releaseCheck = day.hasReleaseToday.length > 0,
+      hasRelease = releaseCheck ? " release-today" : "",
+      releaseTitle = releaseCheck ? day.hasReleaseToday[0].title : "";
 
     return (
       <div
@@ -96,14 +93,13 @@ class Calendar extends React.Component {
         key={day.date.toString()}
       >
         <h3 className="day-label">{day.number}</h3>
-        <div className="release">{(day.hasReleaseToday.length  > 0) ? day.hasReleaseToday[0].title:''  }</div>
+        <div className="release">{releaseTitle}</div>
       </div>
     );
   };
   renderWeek = (day, month) => {
     const days = [];
     let date = day;
-    // console.log(date, "renderWeek", month, release)
     for (var i = 0; i < 7; i++) {
       days.push(this.renderDay(date, month));
       date = clone(m(date).add(1, "d"));
@@ -128,10 +124,7 @@ class Calendar extends React.Component {
       count = 0;
 
     while (!finish) {
-      // console.log(date, monthIndx);
-      weeks.push(
-        this.renderWeek(date, this.state.currentMonth, this.state.release)
-      );
+      weeks.push(this.renderWeek(date, this.state.currentMonth));
       date.add(1, "w");
       finish = count++ > 2 && monthIndx !== date.month();
       monthIndx = date.month();
@@ -152,7 +145,9 @@ class Calendar extends React.Component {
           <div className="week-days">SATURDAY</div>
         </div>
         <div className="calender-cells">{this.renderMonth()}</div>
-        <footer><img src={Logo} alt="" width="150"/></footer>
+        <footer>
+          <img src={Logo} alt="" width="150" />
+        </footer>
       </section>
     );
   }
